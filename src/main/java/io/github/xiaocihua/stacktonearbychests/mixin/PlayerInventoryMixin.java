db25d3b78698585a786046368a85e9c26fcfa5e4
xiaocihua <wangshouyi4520@gmail.com>
2023-04-29 16:30:46 +0800

    Delete unused field Update LibGui version

--- src/main/java/io/github/xiaocihua/stacktonearbychests/mixin/PlayerInventoryMixin.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/mixin/PlayerInventoryMixin.java
@@ -4,15 +4,12 @@ import io.github.xiaocihua.stacktonearbychests.LockedSlots;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerInventory.class)
-public class PlayerInventoryMixin {
-
-    @Shadow public int selectedSlot;
+public abstract class PlayerInventoryMixin {
 
     @Inject(method = "setStack", at = @At("HEAD"))
     private void onSetStack(int slot, ItemStack stack, CallbackInfo ci) {
