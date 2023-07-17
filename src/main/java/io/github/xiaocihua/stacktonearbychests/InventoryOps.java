970b4d6e3ab81a175bcbece2437200e81f0ff685
xiaocihua <wangshouyi4520@gmail.com>
2023-07-17 16:19:00 +0800

    Fix "Items that will not be restocked" list not working properly

--- src/main/java/io/github/xiaocihua/stacktonearbychests/InventoryOps.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/InventoryOps.java
@@ -286,7 +286,7 @@ public class InventoryOps {
         slots.playerSlots().stream()
                 .filter(Slot::hasStack)
                 .filter(slot -> slot.getStack().isStackable())
-                .filter(slot -> !ModOptions.get().behavior.itemsThatWillNotBeStacked
+                .filter(slot -> !ModOptions.get().behavior.itemsThatWillNotBeRestocked
                         .contains(Registries.ITEM
                                 .getId(slot.getStack().getItem())
                                 .toString()))
