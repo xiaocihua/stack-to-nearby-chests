6754b3d554c60bf21c2c2674f5883119a0effc69
xiaocihua <wangshouyi4520@gmail.com>
2023-09-14 22:28:57 +0800

    Now you can quick stack all items of one type to nearby containers via shortcut keys or by dragging and dropping items to the "Quick stack to nearby containers" button

--- src/main/java/io/github/xiaocihua/stacktonearbychests/StackToNearbyChests.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/StackToNearbyChests.java
@@ -12,18 +12,28 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
+import net.fabricmc.fabric.impl.client.screen.ScreenExtensions;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.*;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
+
+import java.util.Optional;
+
+import static java.util.function.Predicate.not;
 
 @Environment(EnvType.CLIENT)
 public class StackToNearbyChests implements ClientModInitializer {
@@ -32,10 +42,13 @@ public class StackToNearbyChests implements ClientModInitializer {
 
     private static final Identifier BUTTON_TEXTURE = new Identifier(ModOptions.MOD_ID, "textures/buttons.png");
 
+    public static Optional<PosUpdatableButtonWidget> currentStackToNearbyContainersButton = Optional.empty();
+
     @Override
     public void onInitializeClient() {
         KeySequence.init();
         LockedSlots.init();
+        InventoryActions.init();
         ForEachContainerTask.init();
 
         ScreenEvents.AFTER_INIT.register(this::addButtonsAndKeys);
@@ -48,7 +61,8 @@ public class StackToNearbyChests implements ClientModInitializer {
     }
 
     private void addButtonsAndKeys(MinecraftClient client, Screen screen, int scaledWidth, int scaledHeight) {
-        if (client.player == null || client.player.isSpectator()) {
+        ClientPlayerEntity player = client.player;
+        if (player == null || player.isSpectator()) {
             return;
         }
 
@@ -57,14 +71,36 @@ public class StackToNearbyChests implements ClientModInitializer {
 
         if (screen instanceof AbstractInventoryScreen<?> inventoryScreen) {
             if (ModOptions.get().appearance.showStackToNearbyContainersButton.booleanValue()) {
-                new PosUpdatableButtonWidget.Builder(inventoryScreen)
+                var buttonWidget = new PosUpdatableButtonWidget.Builder(inventoryScreen)
                         .setUV(0, 0)
                         .setTexture(BUTTON_TEXTURE, 64, 32)
                         .setTooltip(showButtonTooltip ? getTooltipWithHint("stack-to-nearby-chests.tooltip.stackToNearbyContainersButton") : null)
                         .setPosUpdater(parent -> new Vec2i(parent.getX() + appearanceOption.stackToNearbyContainersButtonPosX.intValue(),
                                 parent.getY() + appearanceOption.stackToNearbyContainersButtonPosY.intValue()))
-                        .setPressAction(button -> InventoryActions.stackToNearbyContainers())
+                        .setPressAction(button -> {
+                            ScreenHandler screenHandler = inventoryScreen.getScreenHandler();
+                            ItemStack cursorStack = screenHandler.getCursorStack();
+                            if (cursorStack.isEmpty()) {
+                                InventoryActions.stackToNearbyContainers();
+                            } else {
+                                Item item = cursorStack.getItem();
+
+                                screenHandler.slots.stream()
+                                        .filter(slot -> slot.inventory instanceof PlayerInventory)
+                                        .filter(slot -> slot.getIndex() < 36) // 36: feet, 37: legs, 38: chest, 39: head, 40: offhand
+                                        .filter(not(LockedSlots::isLocked))
+                                        .filter(slot -> !slot.hasStack() || InventoryActions.canMerge(slot.getStack(), cursorStack))
+                                        .peek(slot -> InventoryActions.pickup(screenHandler, slot))
+                                        .anyMatch(slot -> cursorStack.isEmpty());
+
+                                InventoryActions.stackToNearbyContainers(item);
+                            }
+                        })
                         .build();
+
+                currentStackToNearbyContainersButton = Optional.ofNullable(buttonWidget);
+
+                ((ScreenExtensions) screen).fabric_getRemoveEvent().register(s -> currentStackToNearbyContainersButton = Optional.empty());
             }
 
             if (ModOptions.get().appearance.showRestockFromNearbyContainersButton.booleanValue()) {
@@ -78,9 +114,22 @@ public class StackToNearbyChests implements ClientModInitializer {
                         .build();
             }
 
-            ScreenKeyboardEvents.afterKeyPress(screen).register((screen1, key, scancode, modifiers) -> {
-                ModOptions.get().keymap.stackToNearbyContainersKey.testThenRun(InventoryActions::stackToNearbyContainers);
-                ModOptions.get().keymap.restockFromNearbyContainersKey.testThenRun(InventoryActions::restockFromNearbyContainers);
+            ScreenKeyboardEvents.afterKeyPress(screen).register((scr, key, scancode, modifiers) -> {
+                ModOptions.Keymap keymap = ModOptions.get().keymap;
+
+                boolean triggered = false;
+
+                Slot focusedSlot = ((HandledScreenAccessor) inventoryScreen).getFocusedSlot();
+                if (focusedSlot != null && focusedSlot.hasStack()) {
+                    triggered = keymap.quickStackItemsOfTheSameTypeAsTheOneUnderTheCursorToNearbyContainersKey
+                            .testThenRun(() -> InventoryActions.stackToNearbyContainers(focusedSlot.getStack().getItem()));
+                }
+
+                if (!triggered) {
+                    keymap.stackToNearbyContainersKey.testThenRun(InventoryActions::stackToNearbyContainers);
+                }
+
+                keymap.restockFromNearbyContainersKey.testThenRun(InventoryActions::restockFromNearbyContainers);
             });
         } else if (isContainerScreen(screen)) {
             ScreenHandler screenHandler = ((HandledScreen<?>) screen).getScreenHandler();
