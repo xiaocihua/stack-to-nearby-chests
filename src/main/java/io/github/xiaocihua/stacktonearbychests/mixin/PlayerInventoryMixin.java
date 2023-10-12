281261c212ea99bd0327fac1acc090a43eecd50b
xiaocihua <wangshouyi4520@gmail.com>
2023-10-12 23:46:56 +0800

    Fix compatibility issues with Inventory Profiles Next

--- src/main/java/io/github/xiaocihua/stacktonearbychests/mixin/PlayerInventoryMixin.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/mixin/PlayerInventoryMixin.java
@@ -1,9 +1,12 @@
 package io.github.xiaocihua.stacktonearbychests.mixin;
 
 import io.github.xiaocihua.stacktonearbychests.LockedSlots;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,8 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
 
+    @Shadow @Final public PlayerEntity player;
+
     @Inject(method = "setStack", at = @At("HEAD"))
     private void onSetStack(int slot, ItemStack stack, CallbackInfo ci) {
-        LockedSlots.onSetStack(slot, stack);
+        if (player.getWorld().isClient) {
+            LockedSlots.onSetStack(slot, stack);
+        }
     }
 }
