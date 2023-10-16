14c753a2204a9a5f8cd019cfb20b6e057fa00989
xiaocihua <wangshouyi4520@gmail.com>
2023-10-16 19:35:29 +0800

    Fix the bug where key bindings are activated when you type in a text field; The Survival Inventory tab is the only tab of the creative inventory screen where the keybindings of this mod can still be used

--- src/main/java/io/github/xiaocihua/stacktonearbychests/StackToNearbyChests.java
+++ src/main/java/io/github/xiaocihua/stacktonearbychests/StackToNearbyChests.java
@@ -7,6 +7,7 @@ import io.github.xiaocihua.stacktonearbychests.gui.ModOptionsScreen;
 import io.github.xiaocihua.stacktonearbychests.gui.PosUpdatableButtonWidget;
 import io.github.xiaocihua.stacktonearbychests.mixin.HandledScreenAccessor;
 import io.github.xiaocihua.stacktonearbychests.mixin.HorseScreenAccessor;
+import io.github.xiaocihua.stacktonearbychests.mixin.RecipeBookWidgetAccessor;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -15,13 +16,17 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
 import net.fabricmc.fabric.impl.client.screen.ScreenExtensions;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.*;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
@@ -131,6 +136,10 @@ public class StackToNearbyChests implements ClientModInitializer {
             }
 
             ScreenKeyboardEvents.afterKeyPress(screen).register((scr, key, scancode, modifiers) -> {
+                if (isTextFieldActive(scr) || isInventoryTabNotSelected(scr)) {
+                    return;
+                }
+
                 ModOptions.Keymap keymap = ModOptions.get().keymap;
 
                 boolean triggered = false;
@@ -170,11 +179,33 @@ public class StackToNearbyChests implements ClientModInitializer {
                         .build();
             }
 
-            ScreenKeyboardEvents.afterKeyPress(screen).register((screen1, key, scancode, modifiers) -> {
+            ScreenKeyboardEvents.afterKeyPress(screen).register((scr, key, scancode, modifiers) -> {
+                if (isTextFieldActive(scr) || isInventoryTabNotSelected(scr)) {
+                    return;
+                }
+
                 ModOptions.get().keymap.quickStackKey.testThenRun(() -> InventoryActions.quickStack(screenHandler));
                 ModOptions.get().keymap.restockKey.testThenRun(() -> InventoryActions.restock(screenHandler));
             });
         }
+    }
+
+    private static boolean isTextFieldActive(Screen screen) {
+        Element focusedElement = screen.getFocused();
+
+        if (focusedElement instanceof RecipeBookWidget) {
+            TextFieldWidget searchField = ((RecipeBookWidgetAccessor) focusedElement).getSearchField();
+            if (searchField != null && searchField.isActive()) {
+                return true;
+            }
+        }
+
+        return focusedElement instanceof TextFieldWidget textField && textField.isActive();
+    }
+
+    private static boolean isInventoryTabNotSelected(Screen screen) {
+        return screen instanceof CreativeInventoryScreen creativeInventoryScreen
+                && creativeInventoryScreen.getSelectedTab() != ItemGroup.INVENTORY.getIndex();
     }
 
     private static Vec2i getAbsolutePos(HandledScreenAccessor parent, ModOptions.IntOption x, ModOptions.IntOption y) {
